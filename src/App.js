7ddd3a045632df0ac41beca24248c75f646741bc
Tim Zdornov <lighttimfire@MacBook-Air-Tim.local>
2018-12-05 16:54:53 +0300

    Sync local and remote

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Main from './Main.js';
+import './Wrapper.css'
 
-const logus = <img src={logo} className="App-logo" alt="logo" />;
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {logus}
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div class="AppWrapper">
+      <Main />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './Main.css';
+import Aside from './Aside.js';
+
+class Main extends Component {
+  WheelClickFirst = () => {
+    let clickElement = document.getElementsByClassName('MainGroup-PickerWheel_first')[0],
+        displayGroupFirst = document.getElementsByClassName('Maingroup-SkillsSection')[0],
+        displayGroupSecond = document.getElementsByClassName('Maingroup-HackSection')[0],
+        displayGroupThird = document.getElementsByClassName('Maingroup-AboutSection')[0];
+
+        if(clickElement) {
+          displayGroupFirst.style.display = 'flex';
+          displayGroupSecond.style.display = 'none';
+          displayGroupThird.style.display = 'none';
+        }
+  }
+  WheelClickSecond = () => {
+    let clickElement = document.getElementsByClassName('MainGroup-PickerWheel_second')[0],
+        displayGroupFirst = document.getElementsByClassName('Maingroup-SkillsSection')[0],
+        displayGroupSecond = document.getElementsByClassName('Maingroup-HackSection')[0],
+        displayGroupThird = document.getElementsByClassName('Maingroup-AboutSection')[0];
+
+        if(clickElement) {
+          displayGroupFirst.style.display = 'none';
+          displayGroupSecond.style.display = 'flex';
+          displayGroupThird.style.display = 'none';
+        }
+  }
+  WheelClickThird = () => {
+    let clickElement = document.getElementsByClassName('MainGroup-PickerWheel_second')[0],
+        displayGroupFirst = document.getElementsByClassName('Maingroup-SkillsSection')[0],
+        displayGroupSecond = document.getElementsByClassName('Maingroup-HackSection')[0],
+        displayGroupThird = document.getElementsByClassName('Maingroup-AboutSection')[0];
+
+        if(clickElement) {
+          displayGroupFirst.style.display = 'none';
+          displayGroupSecond.style.display = 'none';
+          displayGroupThird.style.display = 'flex';
+        }
+  }
+  render() {
+    return (
+      <main class="MainGroup">
+        <div class="MainGroup-About_wrapper">
+        <Aside />
+        <div class="MainGroup-PickerWheel_wrapper">
+          <div class="MainGroup-PickerWheel">
+            <div onClick={this.WheelClickFirst} class="MainGroup-PickerWheel_first">Soft skills</div>
+            <div onClick={this.WheelClickSecond} class="MainGroup-PickerWheel_second">Hacking</div>
+            <div onClick={this.WheelClickThird} class="MainGroup-PickerWheel_third">About</div>
+            </div>
+          </div>
+          <div class="MainGroup-Initials_wrapper">
+            <h1>Tim Zdornov</h1>
+            <p>Front-end developer</p>
+            <div class="AboutGroup-Logos_wrapper">
+              <a href="https://vk.com/lighttimfire" target="_blank" rel="noopener noreferrer" class="AboutGroup-LogoVk"></a>
+              <a href="https://github.com/dreamchasersuon" target="_blank" rel="noopener noreferrer" class="AboutGroup-LogoGitHub"></a>
+              <a href="https://www.facebook.com/formulatim" target="_blank" rel="noopener noreferrer" class="AboutGroup-LogoFacebook"></a>
+            </div>
+          </div>
+          <div class="AboutWrapper-Photo_wrapper">
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Main;
